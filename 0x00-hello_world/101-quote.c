62aed35af7727f3eb80eaf5bc318b9850c5ac62d
Netshivhambe <netshivhambe17@gmail.com>
2023-06-16 04:56:02 -0700

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - standard error
- * Return 1
+ * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ,stdout);
-return(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 56);
+	return (1);
 }
