84e60fa8896014c98aede18a76512b59cf44b3da
Netshivhambe <netshivhambe17@gmail.com>
2023-06-18 09:55:49 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - numbers in orders
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 0; alpha < 10; alpha++)
+	{
+		putchar(alpha + '0');
+		if (alphaha < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
