86e8cf0cb95231bbad08f2cb2ceefdd321d5a111
Netshivhambe <netshivhambe17@gmail.com>
2023-06-18 07:54:38 -0700

    task9

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - reverse alpha
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha c--)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
