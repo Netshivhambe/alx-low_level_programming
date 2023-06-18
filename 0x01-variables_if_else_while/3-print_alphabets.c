631c983f1015b5ff81a71fe94ca2e3ec749c79a0
Netshivhambe <netshivhambe17@gmail.com>
2023-06-17 22:42:16 -0700

    task7

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print alpha
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+		putchar(alpha);
+	for (alpha = 'a' ; alpha <= 'Z' ; alpha++)
+		putchar(alpha);
+	putchar('\n');
+}
