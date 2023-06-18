5a8441b2e3ba22998c88294c3eb7bc5e63ffa136
Netshivhambe <netshivhambe17@gmail.com>
2023-06-18 00:29:21 -0700

    task8

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,12 +8,10 @@
 int main(void)
 {
 	char alpha;
-	char e;
-	char q;
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-		if (alpha != e && alpha != q)
+		if (alpha != 'e' && alpha != 'q')
 		putchar(alpha);
 	}
 	putchar('\n');
