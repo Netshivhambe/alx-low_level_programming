7c2f8318209c80d2cd691ed26d1b18a6e8187652
Netshivhambe <netshivhambe17@gmail.com>
2023-06-18 00:05:45 -0700

    task8

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 10; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
