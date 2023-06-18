8908064d22ca9f0f24aee3b0aa5b33cabcd447d4
Netshivhambe <netshivhambe17@gmail.com>
2023-06-18 06:34:31 -0700

    task8

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 /**
- * main - numbers
+ * main - programming numbers
  *
  * Return: 0
  */
+
 int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
-		printf("%d", num);
+	for (num = 0; num <= 10; num++)
+		putchar(num);
 	putchar('\n');
 	return (0);
 }
