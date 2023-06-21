11d1da65ac86bcbc6c567bd57c9c3bd934f07d0b
Netshivhambe <netshivhambe17@gmail.com>
2023-06-21 10:05:29 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - numbers printed
+ * @m: Check mumbers
+ * Return: 1 positive, -1 negative
+ */
+
+int print_sign(int m)
+
+{
+	if (m > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (m < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
