da4b0bf74e02929098380ebddc05ab9f7fbf8918
Netshivhambe <netshivhambe17@gmail.com>
2023-06-21 09:14:03 -0700

    task islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - Lowercase check
+ * @c: Check character
+ * Return: 1
+ */
+
+int _islower(int alpha)
+
+{
+	if (alpha >= 97 && alpha <= 122)
+	{
+	return (1);
+	}
+	return (0);
+}
