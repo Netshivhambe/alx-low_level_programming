1d22f3b8c494d9611d8061e708c815c3b9d39299
Netshivhambe <netshivhambe17@gmail.com>
2023-06-21 06:51:24 -0700

    task6

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,11 @@
  * Return: void
  */
 
-void print_alphabet (void)
-
+void print_alphabet(void)
 {
 	char alpha;
+
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
-		_putchar(alpha);
+	_putchar(alpha);
 	_putchar('\n');
 }
